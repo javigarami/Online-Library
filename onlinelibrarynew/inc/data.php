c21fa3f4d50cd132a6e83dc3510621caf4292ef8
javigarami <41347987+javigarami@users.noreply.github.com>
2019-06-03 18:10:35 -0400

    Add files via upload

---
+++ onlinelibrarynew/inc/data.php
@@ -0,0 +1,175 @@
+<?php
+$catalog=[];
+//Books
+$catalog[101] = [
+ "title" => "1984",
+ "img" => "img/media/1984.png",
+ "genre" => "SciFi",
+ "format" => "Paperback",
+ "year" => 1942,
+ "category" => "Books",
+ "authors" => [
+     "George Orwell",
+ ],
+ "publisher" => "Sewick and Warburg",
+ "isbn" => '978-0156012195'
+];
+
+$catalog[102] = [
+ "title" => "Inferno",
+ "img" => "img/media/inferno.jpg",
+ "genre" => "Poem",
+ "format" => "Paperback",
+ "year" => 14,
+ "category" => "Books",
+ "authors" => [
+     "Dante Alighieri",
+ ],
+ "publisher" => "none",
+ "isbn" => 'none'
+];
+
+$catalog[103] = [
+ "title" => "Odyssey",
+ "img" => "img/media/odyssey.png",
+ "genre" => "Epic Adventure",
+ "format" => "Paperback",
+ "year" => Unavailable,
+ "category" => "Books",
+ "authors" => [
+     "Homer",
+ ],
+ "publisher" => "Unavailable",
+ "isbn" => 'Unavailable'
+];
+
+$catalog[104] = [
+ "title" => "Weaving the Web",
+ "img" => "img/media/web.png",
+ "genre" => "Fantasy",
+ "format" => "Paperback",
+ "year" => 1998,
+ "category" => "Books",
+ "authors" => [
+     "Tim Berners Lee",
+ ],
+ "publisher" => "Harper",
+ "isbn" => '00-6-25-158861'
+];
+//Movies
+$catalog[201] = [
+ "title" => "Gladiator",
+ "img" => "img/media/gladiator.png",
+ "genre" => "Adventure",
+ "format" => "DVD",
+ "year" => 2000,
+ "category" => "Movies",
+ "director" => "Ridley Scott",
+ "writers" => [
+     "David Franzoni",
+ ],
+ "stars" => [
+     "Russel Crowe",
+     "Joaquin Phoenix",
+     "Connie Nielsen"
+ ]
+];
+
+$catalog[202] = [
+ "title" => "Black Panther",
+ "img" => "img/media/wakanda.png",
+ "genre" => "Comic Book",
+ "format" => "DVD",
+ "year" => 2018,
+ "category" => "Movies",
+ "director" => "Kevin Feige",
+ "writers" => [
+     "Ryan Coogler",
+
+ ],
+ "stars" => [
+     "Chadwick Boseman",
+     "Michael B Jordan",
+     "Lupita Nyongo'o"
+ ]
+];
+
+$catalog[203] = [
+ "title" => "Dog Day Afternoon",
+ "img" => "img/media/dogday.png",
+ "genre" => "Non Fiction, Drama",
+ "format" => "DVD",
+ "year" => 1975,
+ "category" => "Movies",
+ "director" => ["Sidney Lumet"],
+ "writers" => [
+     "Martin Bregman",
+	 "Frank Pierson"
+ ],
+ "stars" => [
+     "Al Pacino",
+     "John Cazale",
+     "James Broderick"
+ ]
+];
+
+$catalog[204] = [
+ "title" => "Starship Troopers",
+ "img" => "img/media/starship.png",
+ "genre" => "Comedy",
+ "format" => "DVD",
+ "year" => 1997,
+ "category" => "Movies",
+ "director" => "Paul Verhoeven",
+ "writers" => [
+     "",
+	 "Edward Neumeier"
+ ],
+ "stars" => [
+     "Casper Van Dien",
+     "Dina Meyer",
+     "Denise Richards"
+ ]
+];
+
+//Music
+$catalog[301] = [
+ "title" => "Audioslave",
+ "img" => "img/media/audioslave.png",
+ "genre" => "Alternative",
+ "format" => "Digital",
+ "year" => 2002,
+ "category" => "Music",
+ "artist" => "AudioSlave"
+];
+
+$catalog[302] = [
+ "title" => "Best of Hall and Oates",
+ "img" => "img/media/hallnoates.png",
+ "genre" => "Pop Rock",
+ "format" => "Digital",
+ "year" => 1988,
+ "category" => "Music",
+ "artist" => "John Hall and Daryl Oates"
+];
+
+$catalog[303] = [
+ "title" => "Illmatic",
+ "img" => "img/media/illmatic.png",
+ "genre" => "Rap",
+ "format" => "Digital",
+ "year" => 1994,
+ "category" => "Music",
+ "artist" => "NAS"
+];
+
+$catalog[304] = [
+ "title" => "Gran Combo",
+ "img" => "img/media/gran.png",
+ "genre" => "Salsa",
+ "format" => "Digital",
+ "year" => 1962,
+ "category" => "Music",
+ "artist" => "El Gran Combo De PR"
+];
+?>
